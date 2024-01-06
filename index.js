1a311a5bf8866c1fe5864953454b6eb4879dbd65
brandon <brandon7.7porcel@gmail.com>
2024-01-06 16:39:28 -0300

    feat: retoranar html template

--- index.js
+++ index.js
@@ -156,8 +156,122 @@ app.post("/scrape", async (req, res) => {
   }
 });
 
+const isAlbum = (url) => {
+  return url.split("/")[3].includes("albums");
+};
+
+const getPageTitle = async (page) => {
+  const pageTitleEl = await page.$("h1");
+  if (!pageTitleEl) {
+    throw new Error("No se encontró el elemento h1 en la página.");
+  }
+
+  return await pageTitleEl.innerText();
+};
+
+const getSongTemplate = async (page) => {
+  await page.waitForSelector("#lyrics-root-pin-spacer");
+
+  await page.waitForSelector("h1");
+  const title = await page.evaluate(
+    () => document.querySelector("h1").outerHTML
+  );
+
+  const lyricsSections = await page.evaluate(() => {
+    const lyricsPartsNodeList = document.querySelectorAll(
+      "#lyrics-root-pin-spacer div[data-lyrics-container]"
+    );
+
+    const lyricsParts = [];
+    for (let part of lyricsPartsNodeList) {
+      lyricsParts.push(part.outerHTML);
+    }
+    return lyricsParts;
+  });
+
+  const htmlTemplate = `
+    ${title}${lyricsSections.map((el) => el + "<br>").join("")}
+  `;
+
+  return htmlTemplate;
+};
+
+const getAlbumTemplate = async (page) => {
+  await page.waitForSelector("album-tracklist-row a");
+  const links = await page.evaluate(() => {
+    const items = document.querySelectorAll("album-tracklist-row a");
+
+    const links = [];
+    for (let item of items) {
+      links.push(item.href);
+    }
+    return links;
+  });
+
+  let template = "";
+
+  for (let link of links) {
+    await page.goto(link, { waitUntil: "domcontentloaded" });
+    const songTemplate = await getSongTemplate(page);
+    template += songTemplate;
+
+    await page.waitForTimeout(1000);
+  }
+
+  return template;
+};
+
+const generateHtmlTemplate = (template) => {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Kindle-Genius</title>
+    </head>
+    <body>
+      ${template}
+    </body>
+    </html>
+  `;
+};
+
 const scrapeAndGeneratePDF = async (userURL) => {
-  return "hola";
+  const browser = await chromium.launch({
+    headless: true,
+  });
+
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  try {
+    if (!userURL) {
+      return "Necesito la URL como argumento.";
+    }
+
+    const isAnAlbum = isAlbum(userURL);
+
+    await page.goto(userURL, {
+      waitUntil: "domcontentloaded",
+    });
+
+    const pdfTitle = await getPageTitle(page);
+    let template = "";
+
+    if (isAnAlbum) {
+      template = await getAlbumTemplate(page);
+    } else {
+      template = await getSongTemplate(page);
+    }
+
+    const newHtmlContent = generateHtmlTemplate(template);
+    await context.close();
+    await browser.close();
+    return newHtmlContent;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 };
 
 app.listen(port, () => {
